11893d731df38f4d2d37a08427bc8bf9f4ad8e0a
Denis Bystruev <dbystruev@me.com>
2025-12-14 18:21:44 -0800

    Add shared types from conect

---
+++ conect-shared-types.ts
@@ -0,0 +1,25 @@
+// Auto-generated shared types
+export interface Gridstatus {
+  id: string;
+  // TODO: Add fields
+}
+
+export interface Simulatestressevent {
+  id: string;
+  // TODO: Add fields
+}
+
+export interface Reportsavings {
+  id: string;
+  // TODO: Add fields
+}
+
+export interface Regulate {
+  id: string;
+  // TODO: Add fields
+}
+
+export interface Health {
+  id: string;
+  // TODO: Add fields
+}
